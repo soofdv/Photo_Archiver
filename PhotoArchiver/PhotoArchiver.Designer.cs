f579da032e2badf4d8e7cdb77f907b0ca4992c37
Sven Steggerda <svenradius@hotmail.com>
2019-09-24 15:26:26 +0200

    BeginScherm

---
+++ PhotoArchiver/PhotoArchiver.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace PhotoArchiver
+{
+    partial class photoArchiverForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileLoaderButton = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.fileNameTextbox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.optionsButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // fileLoaderButton
+            // 
+            this.fileLoaderButton.Location = new System.Drawing.Point(13, 13);
+            this.fileLoaderButton.Name = "fileLoaderButton";
+            this.fileLoaderButton.Size = new System.Drawing.Size(248, 36);
+            this.fileLoaderButton.TabIndex = 0;
+            this.fileLoaderButton.Text = "Bestanden Inladen";
+            this.fileLoaderButton.UseVisualStyleBackColor = true;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(13, 55);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(248, 422);
+            this.treeView1.TabIndex = 1;
+            // 
+            // fileNameTextbox
+            // 
+            this.fileNameTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileNameTextbox.Location = new System.Drawing.Point(278, 12);
+            this.fileNameTextbox.Name = "fileNameTextbox";
+            this.fileNameTextbox.Size = new System.Drawing.Size(279, 30);
+            this.fileNameTextbox.TabIndex = 2;
+            this.fileNameTextbox.Text = "Bestandsnaam";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(563, 12);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(115, 30);
+            this.saveButton.TabIndex = 3;
+            this.saveButton.Text = "Opslaan";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(278, 48);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(400, 33);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(278, 87);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(400, 390);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // optionsButton
+            // 
+            this.optionsButton.Location = new System.Drawing.Point(694, 12);
+            this.optionsButton.Name = "optionsButton";
+            this.optionsButton.Size = new System.Drawing.Size(77, 30);
+            this.optionsButton.TabIndex = 6;
+            this.optionsButton.Text = "Opties";
+            this.optionsButton.UseVisualStyleBackColor = true;
+            // 
+            // photoArchiverForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(783, 489);
+            this.Controls.Add(this.optionsButton);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.fileNameTextbox);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.fileLoaderButton);
+            this.Name = "photoArchiverForm";
+            this.Text = "Photo Archiver";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button fileLoaderButton;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox fileNameTextbox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button optionsButton;
+    }
+}
+
---
+++ PhotoArchiver/PhotoArchiver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhotoArchiver
+{
+    public partial class photoArchiverForm : Form
+    {
+        public photoArchiverForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
