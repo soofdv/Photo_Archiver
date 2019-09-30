b087c5af06c201fd41fb8a65686be0f0e3db4166
Sofie <sova@live.nl>
2019-09-30 11:16:08 +0200

    begin

--- PhotoArchiver/Picture.cs
+++ PhotoArchiver/Picture.cs
@@ -1,13 +1,28 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 
 namespace PhotoArchiver
 {
-    internal class Picture
+    public class Picture
     {
+        public string id { get; set; }
         public string PictureName { get; set; }
+        public string item_type { get; set; }
         public DateTime DateTimeCreated { get; set; }
         public string FilePath { get; set; }
-        public string itemType { get; set; }
-        public string parentDirPath { get; set; }
+        public string folder_id { get; set; }
+    
+    }
+    public class PhotoInformation
+    {
+        public List<Picture> pictureInfo = new List<Picture>();
+
+        public void getData()
+        {
+            System.Net.WebClient downloader = new System.Net.WebClient();
+            string dataJson = downloader.DownloadString("http://localhost/php/photo_archiver/read.php");//hier moet je zelf ff kijken hoe de link er uitziet bij je.
+            pictureInfo = JsonConvert.DeserializeObject<List<Picture>>(dataJson);
+        }
     }
 }
