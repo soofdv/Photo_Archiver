e134bfde5b1b87f8fa8d4e7db66429d8ed6a0031
Sven Steggerda <svenradius@hotmail.com>
2019-09-30 08:46:53 +0200

    Hernoemen

--- PhotoArchiver/PhotoArchiver.Designer.cs
+++ PhotoArchiver/PhotoArchiver.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace PhotoArchiver
+namespace PhotoArchiver
 {
     partial class photoArchiverForm
     {
@@ -32,15 +32,15 @@
             this.fileOverview = new System.Windows.Forms.TreeView();
             this.fileNameTextbox = new System.Windows.Forms.TextBox();
             this.saveButton = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.formatsCombobox = new System.Windows.Forms.ComboBox();
+            this.previewListbox = new System.Windows.Forms.ListView();
             this.optionsButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // fileLoaderButton
             // 
             this.fileLoaderButton.Location = new System.Drawing.Point(10, 11);
-            this.fileLoaderButton.Margin = new System.Windows.Forms.Padding(2);
+            this.fileLoaderButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.fileLoaderButton.Name = "fileLoaderButton";
             this.fileLoaderButton.Size = new System.Drawing.Size(186, 29);
             this.fileLoaderButton.TabIndex = 0;
@@ -51,55 +51,59 @@
             // fileOverview
             // 
             this.fileOverview.Location = new System.Drawing.Point(10, 45);
-            this.fileOverview.Margin = new System.Windows.Forms.Padding(2);
+            this.fileOverview.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.fileOverview.Name = "fileOverview";
             this.fileOverview.Size = new System.Drawing.Size(186, 344);
             this.fileOverview.TabIndex = 1;
+            this.fileOverview.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.fileOverview_AfterSelect);
             // 
             // fileNameTextbox
             // 
             this.fileNameTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.fileNameTextbox.Location = new System.Drawing.Point(208, 10);
-            this.fileNameTextbox.Margin = new System.Windows.Forms.Padding(2);
+            this.fileNameTextbox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.fileNameTextbox.Name = "fileNameTextbox";
             this.fileNameTextbox.Size = new System.Drawing.Size(210, 26);
             this.fileNameTextbox.TabIndex = 2;
             this.fileNameTextbox.Text = "Bestandsnaam";
+            this.fileNameTextbox.TextChanged += new System.EventHandler(this.fileNameTextbox_TextChanged);
             // 
             // saveButton
             // 
             this.saveButton.Location = new System.Drawing.Point(422, 10);
-            this.saveButton.Margin = new System.Windows.Forms.Padding(2);
+            this.saveButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.saveButton.Name = "saveButton";
             this.saveButton.Size = new System.Drawing.Size(86, 24);
             this.saveButton.TabIndex = 3;
             this.saveButton.Text = "Opslaan";
             this.saveButton.UseVisualStyleBackColor = true;
             // 
-            // comboBox1
+            // formatsCombobox
             // 
-            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(208, 39);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(301, 28);
-            this.comboBox1.TabIndex = 4;
+            this.formatsCombobox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.formatsCombobox.FormattingEnabled = true;
+            this.formatsCombobox.Location = new System.Drawing.Point(208, 39);
+            this.formatsCombobox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.formatsCombobox.Name = "formatsCombobox";
+            this.formatsCombobox.Size = new System.Drawing.Size(301, 28);
+            this.formatsCombobox.TabIndex = 4;
+            this.formatsCombobox.SelectedIndexChanged += new System.EventHandler(this.formatsCombobox_SelectedIndexChanged);
             // 
-            // listView1
+            // previewListbox
             // 
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(208, 71);
-            this.listView1.Margin = new System.Windows.Forms.Padding(2);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(301, 318);
-            this.listView1.TabIndex = 5;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.previewListbox.HideSelection = false;
+            this.previewListbox.Location = new System.Drawing.Point(208, 71);
+            this.previewListbox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.previewListbox.Name = "previewListbox";
+            this.previewListbox.Size = new System.Drawing.Size(301, 318);
+            this.previewListbox.TabIndex = 5;
+            this.previewListbox.UseCompatibleStateImageBehavior = false;
+            this.previewListbox.View = System.Windows.Forms.View.List;
             // 
             // optionsButton
             // 
             this.optionsButton.Location = new System.Drawing.Point(520, 10);
-            this.optionsButton.Margin = new System.Windows.Forms.Padding(2);
+            this.optionsButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.optionsButton.Name = "optionsButton";
             this.optionsButton.Size = new System.Drawing.Size(58, 24);
             this.optionsButton.TabIndex = 6;
@@ -113,15 +117,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(587, 397);
             this.Controls.Add(this.optionsButton);
-            this.Controls.Add(this.listView1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.previewListbox);
+            this.Controls.Add(this.formatsCombobox);
             this.Controls.Add(this.saveButton);
             this.Controls.Add(this.fileNameTextbox);
             this.Controls.Add(this.fileOverview);
             this.Controls.Add(this.fileLoaderButton);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "photoArchiverForm";
             this.Text = "Photo Archiver";
+            this.Load += new System.EventHandler(this.photoArchiverForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -133,8 +138,8 @@
         private System.Windows.Forms.TreeView fileOverview;
         private System.Windows.Forms.TextBox fileNameTextbox;
         private System.Windows.Forms.Button saveButton;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ComboBox formatsCombobox;
+        private System.Windows.Forms.ListView previewListbox;
         private System.Windows.Forms.Button optionsButton;
     }
 }
